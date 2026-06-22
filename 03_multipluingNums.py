405db5702df8682c9956ed093b9cfec50b852b6e
TheBoBa2 <glooplootloo@gmail.com>
2026-06-22 21:32:02 +0300

    added task 03

---
+++ 03_multipluingNums.py
@@ -0,0 +1,10 @@
+﻿import math
+
+input_num = int(input("Enter your number: "))
+
+while input_num > 9:
+    numbers = [int(numbers) for numbers in str(input_num)]
+    input_num = math.prod(numbers)
+
+print(input_num)
+ 
