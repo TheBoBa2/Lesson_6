89b72922da534c7d7a0807c44060b5c9438aa43d
TheBoBa2 <glooplootloo@gmail.com>
2026-06-22 21:43:12 +0300

    added task 01

---
+++ 01_lettersRange.py
@@ -0,0 +1,10 @@
+﻿import string
+
+a, b = input().split('-')
+
+alphabet = string.ascii_letters
+
+start = alphabet.index(a)
+end = alphabet.index(b)
+
+print(alphabet[start:end + 1])
--- 03_multipluingNums.py
+++ 03_multipluingNums.py
@@ -3,8 +3,7 @@
 input_num = int(input("Enter your number: "))
 
 while input_num > 9:
-    numbers = [int(numbers) for numbers in str(input_num)]
+    numbers = [int(n) for n in str(input_num)]
     input_num = math.prod(numbers)
 
 print(input_num)
- 
